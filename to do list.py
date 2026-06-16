786af0c20d7e3acfa05ecf721b86d7382535fa4b
Divya-ai-maker <divyadharshini260907@gmail.com>
2026-06-16 17:31:20 +0530

    Create to do list.py

---
+++ to do list.py
@@ -0,0 +1,23 @@
+tasks = []
+while True:
+    print("===== TO-DO LIST =====")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        task = input("Enter a task: ")
+        tasks.append(task)
+        print("Task added successfully!")
+    elif choice == "2":
+        if len(tasks) == 0:
+            print("No tasks available.")
+        else:
+            print("Your Tasks:")
+            for i in range(len(tasks)):
+                print(i + 1, ".", tasks[i])
+    elif choice == "3":
+        print("Thank you for using To-Do List!")
+        break
+    else:
+        print("Invalid choice. Try again.")
